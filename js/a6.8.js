885292f68a73705963f26b2f4469fdc0650a58f6
ScorpionMB <68664263+ScorpionMB@users.noreply.github.com>
2020-07-30 20:28:20 +0300

    Add files via upload

---
+++ js/a6.8.js
@@ -0,0 +1,31 @@
+
+value = 5;
+value_percent = value;
+$("#my-progress-bar").width(value_percent + '%');
+$("#my-progress-bar").text(value_percent + '%');
+
+const btnFirst = $('.j-first');
+
+btnFirst.click(() => {
+	value_percent = value_percent + 1;
+	$("#my-progress-bar").width(value_percent + '%');
+  $("#my-progress-bar").text(value_percent + '%');
+});
+
+const btnSecond = $('.j-second');
+
+btnSecond.click(() => {
+	value_percent = value_percent + 3;
+	$("#my-progress-bar").width(value_percent + '%');
+  $("#my-progress-bar").text(value_percent + '%');
+});
+
+const btnThird = $('.j-third');
+
+btnThird.click(() => {
+	value_percent = value_percent + 7;
+	$("#my-progress-bar").width(value_percent + '%');
+  $("#my-progress-bar").text(value_percent + '%');
+});
+
+
